16e299b09bf85ab542d3e9a22442b8304170b403
Andrea Diaz <andrea.diaz.gardini@gmail.com>
2019-04-25 05:55:14 -0500

    circular working without merging  working

--- circular.h
+++ circular.h
@@ -8,52 +8,186 @@ class CircularLinkedList : public List<T> {
     public:
         CircularLinkedList() : List<T>() {}
 
+        bool empty() {
+          Node<T>* tmp = this->head;
+
+          if (tmp == NULL) {
+            return true;
+          }
+
+          else {
+            return false;
+          }
+        }
+
         T front() {
-            // TODO
+          Node<T>* tmp = this->head;
+
+          return tmp->data;
         }
 
         T back() {
-            // TODO
+          Node<T>* tmp = this->head;
+
+          while (tmp->next != this->head) {
+            tmp = tmp->next;
+          }
+
+          return tmp->data;
         }
 
         void push_front(T value) {
-            // TODO
+          Node<T>* tmp = this->head;
+          Node<T>* node = new Node<T>;
+
+          node->data = value;
+          node->next = this->head;
+          node->prev = this->tail;
+
+          if (tmp != NULL) {
+            while (tmp->next != this->head) {
+              tmp = tmp->next;
+            }
+            tmp->next = node;
+          }
+
+          else {
+            node->next = node;
+          }
+
+          this->head = node;
         }
 
         void push_back(T value) {
-            // TODO
+          if (empty()) {
+            push_front(value);
+          }
+
+          else {
+            Node<T>* node = new Node<T>;
+
+            node->data = value;
+            node->next = this->head;
+            node->prev = this->tail;
+
+            Node<T>* tmp = this->head;
+
+            while (tmp->next != this->head) {
+              tmp = tmp->next;
+            }
+
+            tmp->next = node;
+            node->prev = tmp;
+            this->tail = node;
+          }
         }
 
         void pop_front() {
-            // TODO
+          Node<T>* tmp = this->head;
+          Node<T>* node = this->head;
+          while(tmp->next != this->head) {
+            tmp = tmp->next;
+          }
+
+          this->head = node->next;
+          tmp->next = this->head;
+          free(node);
         }
 
         void pop_back() {
-            // TODO
+          Node<T>* tmp = this->head;
+          Node<T>* aux;
+
+          while (tmp->next->next != this->head) {
+            tmp = tmp->next;
+          }
+
+          aux = tmp->next;
+          tmp->next = this->head;
+          free(tmp);
         }
 
         T operator[](int index) {
-            // TODO
-        }
+          int i = 0;
+          Node<T>* node = this->head;
 
-        bool empty() {
-            // TODO
+          for (node; node != NULL; node = node->next) {
+            if (i == index) {
+              return node->data;
+            }
+
+            i++;
+          }
         }
 
         int size() {
-            // TODO
+          int count = 0;
+          Node<T>* tmp = this->head;
+
+          if (tmp == NULL) {
+            return count;
+          }
+          count ++;
+
+          while (tmp->next != this->head) {
+            count ++;
+            tmp = tmp->next;
+          }
+
+          return count;
         }
 
         void clear() {
-            // TODO
+          Node<T>* tmp = this->head;
+          Node<T>* aux;
+
+          do {
+            aux = tmp;
+            tmp = tmp->next;
+            free(aux);
+          } while (tmp != this->head);
+
+          this->head = NULL;
         }
 
         void sort() {
-            // TODO
+          int count = size();
+          int min;
+          Node<T>* tmp = this->head;
+
+          for (int i = 0; i < count; i++) {
+            while (tmp->next !=this->head) {
+              if (tmp->data > tmp->next->data) {
+                min = tmp->data;
+                tmp->data = tmp->next->data;
+                tmp->next->data = min;
+                tmp = tmp->next;
+              }
+
+              else {
+                tmp = tmp->next;
+              }
+            }
+
+            tmp = this->head;
+          }
+
         }
 
         void reverse() {
-            // TODO
+          Node<T>* tmp = this->head;
+          Node<T>* bef = NULL;
+          Node<T>* next;
+
+          do {
+            next = tmp->next;
+            tmp->next = bef;
+            bef = tmp;
+            tmp = next;
+          } while (tmp != this->head);
+
+          this->head->next = bef;
+          this->head = bef;
         }
 
         string name() {
@@ -61,15 +195,40 @@ class CircularLinkedList : public List<T> {
         }
 
         BidirectionalIterator<T> begin() {
-            // TODO
+          return BidirectionalIterator<T>(this->head->next);
         }
 
 	       BidirectionalIterator<T> end() {
-            // TODO
+           return BidirectionalIterator<T>(this->tail);
         }
 
         void merge(CircularLinkedList<T> list) {
-            // TODO
+        //   CircularLinkedList<T>* aux_list = new CircularLinkedList<T>;
+        //   Node<T>* tmp_list = this->head;
+        //   Node<T>* tmp_list1 = list.head;
+        //
+        //   while (tmp_list !=  && tmp_list1 != NULL) {
+        //     if (tmp_list1->data > tmp_list->data) {
+        //       aux_list->push_back(tmp_list->data);
+        //       tmp_list = tmp_list->next;
+        //     }
+        //
+        //     else {
+        //       aux_list->push_back(tmp_list1->data);
+        //       tmp_list1 = tmp_list1->next;
+        //     }
+        //   }
+        //
+        // while (tmp_list != NULL) {
+        //   aux_list->push_back(tmp_list->data);
+        //   tmp_list = tmp_list->next;
+        // }
+        //
+        // while (tmp_list1 != NULL) {
+        //   aux_list->push_back(tmp_list1->data);
+        //   tmp_list1 = tmp_list1->next;
+        // }
+        // this->head = aux_list->head;
         }
 };
 
